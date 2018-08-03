ac8aa9dee0276a1c92528bc7069e68a30d107b7d
AleksAttanasio <aleks.attanasio@gmail.com>
2018-08-03 08:10:50 -0400

    Project created

---
+++ cmake-build-debug/CMakeFiles/tissueRetraction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tissueRetraction.dir/main.cpp.o"
+  "tissueRetraction.pdb"
+  "tissueRetraction"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tissueRetraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
