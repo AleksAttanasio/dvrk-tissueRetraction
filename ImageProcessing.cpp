8ac4f44a63f388329cf40df67a479e838c7d5bb2
AleksAttanasio <aleks.attanasio@gmail.com>
2018-08-07 06:45:36 -0400

    Added equalizeRGBImage(...) method of ImageProcessing class

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "ImageProcessing.h"
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/highgui/highgui.hpp"
+
+Mat ImageProcessing::equalizeRGBImage(Mat src) {
+
+    if(src.channels() >= 3)
+    {
+        Mat ycrcb;
+
+        cvtColor(src,ycrcb,CV_BGR2YCrCb);
+
+        vector<Mat> channels;
+        split(ycrcb,channels);
+
+        equalizeHist(channels[0], channels[0]);
+
+        Mat result;
+        merge(channels,ycrcb);
+
+        cvtColor(ycrcb,result,CV_YCrCb2BGR);
+
+        return result;
+    }
+    return Mat();
+}
--- ImageProcessing.h
+++ ImageProcessing.h
@@ -5,12 +5,16 @@
 #ifndef TISSUERETRACTION_IMAGEPROCESSING_H
 #define TISSUERETRACTION_IMAGEPROCESSING_H
 
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
+using namespace std;
 
 class ImageProcessing {
+public:
+    Mat equalizeRGBImage(Mat src);
 
 };
-
 
 
 #endif //TISSUERETRACTION_IMAGEPROCESSING_H
