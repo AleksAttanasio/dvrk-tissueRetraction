b6877b169d80064867534c8f0df792e132d78b98
Aleks Attanasio <aleks.attanasio@gmail.com>
2018-10-01 11:40:39 -0400

    Added ROS code

---
+++ cmake-build-debug/CMakeFiles/stereo_img_reader.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/osboxes/catkin_ws/src/dvrk-tissueRetraction/src/ros/stereoImagesReader.cpp" "/home/osboxes/catkin_ws/src/dvrk-tissueRetraction/cmake-build-debug/CMakeFiles/stereo_img_reader.dir/src/ros/stereoImagesReader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_PACKAGE_NAME=\"dvrk-tissueRetraction\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ros/kinetic/include"
+  "/opt/ros/kinetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/opt/opencv-3.4.3/build"
+  "/opt/opencv-3.4.3/include"
+  "/opt/opencv-3.4.3/include/opencv"
+  "/opt/opencv-3.4.3/modules/core/include"
+  "/opt/opencv-3.4.3/modules/flann/include"
+  "/opt/opencv-3.4.3/modules/imgproc/include"
+  "/opt/opencv-3.4.3/modules/ml/include"
+  "/opt/opencv-3.4.3/modules/objdetect/include"
+  "/opt/opencv-3.4.3/modules/photo/include"
+  "/opt/opencv-3.4.3/modules/video/include"
+  "/opt/opencv-3.4.3/modules/viz/include"
+  "/opt/opencv-3.4.3/modules/dnn/include"
+  "/opt/opencv-3.4.3/modules/imgcodecs/include"
+  "/opt/opencv-3.4.3/modules/shape/include"
+  "/opt/opencv-3.4.3/modules/videoio/include"
+  "/opt/opencv-3.4.3/modules/highgui/include"
+  "/opt/opencv-3.4.3/modules/superres/include"
+  "/opt/opencv-3.4.3/modules/ts/include"
+  "/opt/opencv-3.4.3/modules/features2d/include"
+  "/opt/opencv-3.4.3/modules/calib3d/include"
+  "/opt/opencv-3.4.3/modules/stitching/include"
+  "/opt/opencv-3.4.3/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/ros/videoStreamDummy.cpp
@@ -0,0 +1,38 @@
+// ROS and OpenCV
+#include <ros/ros.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+// ROS Messages
+#include "std_msgs/String.h"
+#include <sensor_msgs/image_encodings.h>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+    int pub_rate = 10;
+    std_msgs::String topic_name;
+    topic_name.data = "/camera_dummy";
+    ros::init(argc, argv, "dummies");
+    ros::NodeHandle nh;
+    cv_bridge::CvImage cv_image;
+
+    cv_image.image = cv::imread("resources/images/dummies_test/dvrk.jpg",CV_LOAD_IMAGE_COLOR);
+    cv_image.encoding = "bgr8";
+    sensor_msgs::Image ros_image;
+    cv_image.toImageMsg(ros_image);
+
+    ros::Publisher pub = nh.advertise<sensor_msgs::Image>(topic_name.data, 1);
+    ros::Rate loop_rate(pub_rate);
+    ROS_INFO("Publishing sample image on %s at %d", topic_name.data.c_str(), pub_rate);
+
+    while (nh.ok())
+    {
+        pub.publish(ros_image);
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
