4bce4c50b7f30ef2fa2316bf6865031a618a865a
Aleks Attanasio <aleks.attanasio@gmail.com>
2018-10-02 12:56:49 -0400

    Fixed image path and ROS_INFO message

--- src/ros/stereoImagesReader.cpp
+++ src/ros/stereoImagesReader.cpp
@@ -14,32 +14,37 @@ using namespace std;
 
 cv_bridge::CvImagePtr cv_ptr (new cv_bridge::CvImage);
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+void imageCallback(const sensor_msgs::ImageConstPtr msg) {
 
-    try
-    {
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
-    }
+    try {
 
+         cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+         cv::waitKey(30);
+    }
+    catch (cv_bridge::Exception& e) {
+
+        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+
+    }
 }
 
 int main(int argc, char **argv){
 
+    string sub_topic_name = "/camera_dummy/image";
     ros::init(argc, argv, "stereo_img_reader");
     ros::NodeHandle nh;
+    cv::namedWindow("view");
+    cv::startWindowThread();
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber images_sub = it.subscribe("/camera_dummy", 1, &imageCallback);
+    image_transport::Subscriber images_sub = it.subscribe("/camera_dummy/image", 1, &imageCallback);
     stringstream ss;
 
+    if(cv_ptr->image.size > 0){
+
+        ROS_INFO("Image read successfully from topic %s", sub_topic_name.c_str());
+    }
 
     cv::imwrite( "1.jpg",  cv_ptr->image);
-
-
     ros::spin();
     return 0;
 }
