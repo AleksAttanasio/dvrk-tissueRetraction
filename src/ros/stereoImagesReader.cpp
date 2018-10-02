e3f4f61de984c33bc7a7791c2793496cc5a0e64a
Aleks Attanasio <aleks.attanasio@gmail.com>
2018-10-02 11:08:31 -0400

    Modified subscriber name and added imwrite routine

--- src/ros/stereoImagesReader.cpp
+++ src/ros/stereoImagesReader.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
+#include <sstream>
 
 // ROS Messages
 #include "std_msgs/String.h"
@@ -11,7 +12,19 @@
 
 using namespace std;
 
+cv_bridge::CvImagePtr cv_ptr;
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
 
 }
 
@@ -19,10 +32,12 @@ int main(int argc, char **argv){
 
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub_images = it.subscribe("/camera_dummy", 1, imageCallback);
+    image_transport::Subscriber images_sub = it.subscribe("/camera_dummy", 1, &imageCallback);
+    stringstream ss;
 
+    ss << "1";
 
-
-
+    ROS_ASSERT( cv::imwrite( ss.str(),  cv_ptr->image ) );
+    
     return 0;
 }
