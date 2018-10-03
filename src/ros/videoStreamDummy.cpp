850a653c892486217956b78f7e50fae160bf09b8
Aleks Attanasio <aleks.attanasio@gmail.com>
2018-10-03 07:06:49 -0400

    Minor fixes and code clean up

--- src/ros/videoStreamDummy.cpp
+++ src/ros/videoStreamDummy.cpp
@@ -16,24 +16,22 @@ using namespace std;
 int main(int argc, char **argv){
 
     int pub_rate = 10;
+    string node_name = "camera_dummy";
     string topic_name = "/camera_dummy/image";
     string path = "resources/images/dummies_test/laparoscopy.jpg";
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
 
-    ros::init(argc, argv, "camera_dummy");
+    ros::init(argc, argv, node_name);
     ros::NodeHandle nh;
-
     image_transport::ImageTransport it(nh);
     image_transport::Publisher img_pub = it.advertise(topic_name, 1);
 
-    cv_bridge::CvImage cv_image;
     cv::Mat image = cv::imread(path, CV_LOAD_IMAGE_COLOR);
     cv::waitKey(30);
     sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
 
     ros::Rate loop_rate(pub_rate);
-
-    char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
 
     ROS_INFO("Publishing sample image on %s at %d Hz \nFile: %s \nCWS: %s", topic_name.c_str(), pub_rate, path.c_str(), cwd);
 
