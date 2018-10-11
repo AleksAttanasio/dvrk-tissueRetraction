3c9d5d45ddca75589b44dc60839bd0bc7b0a4f9b
Aleks Attanasio <aleks.attanasio@gmail.com>
2018-10-11 17:28:42 +0100

    User can now enter a custom file name

--- src/ros/kinematicDataReader.cpp
+++ src/ros/kinematicDataReader.cpp
@@ -47,36 +47,49 @@ void kinDataCallback(const sensor_msgs::JointState msg){
 
 int main(int argc, char **argv){
 
+    // initialize window
+    initscr();
+    nocbreak();
+    echo();
+
+    // Welcome message
+    printw("**** KINEMATIC DATA RECORDER ****\n");
+
+    // Variable initialization
     int sample_count = 0;
     int interval_sample_print = 20;
     string node_name = "kinematic_data_reader";
     string kin_data_source_topic_name = "/dvrk/PSM1/io/joint_position";
+    char file_name[100];
     ofstream collection_txt;
     stringstream ss;
-    collection_txt.open("trial_0.txt");
 
-    initscr();
+    // Ask the user the name of the file he wants to print on
+    printw("Enter now the file name to save your trial. NOTE: the suggested format is <file_name> + \".txt\"\n");
+    getstr(file_name);
+
+    collection_txt.open(file_name);
+
+    // Reset keyboard settings
     cbreak();
     noecho();
     nodelay(stdscr, TRUE);
     scrollok(stdscr, TRUE);
 
+    // Node Init
     ros::init(argc, argv, node_name);
     ros::NodeHandle nh;
-
     ros::Subscriber kin_data_sub = nh.subscribe(kin_data_source_topic_name, 1, &kinDataCallback);
-
     ros::Rate rate(30);
-
     sleep(1);
 
-    printw("**** KINEMATIC DATA RECORDER ****\n"
-           "To start record a trial press \"A\". To quit press \"Q\". Trials will be saved automatically once started. \n");
+    printw("To start record a trial press \"A\". To quit press \"Q\". Trials will be saved automatically once finished. \n");
 
     while(ros::ok()){
 
         sleep(1);
 
+        // If user pressed "A" button
         if(keyHit() == 1){
 
             printw("---> STARTED NEW RECORDING SESSION\n");
@@ -86,6 +99,7 @@ int main(int argc, char **argv){
                 // Output number of samples recorded with interval given by interval_sample_print
                 if(sample_count % interval_sample_print == 0) {printw("Recorded: %d samples\n", sample_count);}
 
+                // Save joint_values[]
                 ss << sample_count;
 
                 collection_txt << ss.str().c_str() << "\t" <<
@@ -104,6 +118,7 @@ int main(int argc, char **argv){
             }
         }
 
+        // If user press "Q"
         if (keyHit() == 0){
 
             ROS_INFO("*** TRIAL TERMINATED: %d samples were recorded.", sample_count);
