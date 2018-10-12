22e78de7ee8b29efa47e09f6165f67220b236cc5
Aleks Attanasio <aleks.attanasio@gmail.com>
2018-10-12 12:24:58 +0100

    Initialized node and publisher

--- src/ros/jointStateDummy.cpp
+++ src/ros/jointStateDummy.cpp
@@ -3,8 +3,21 @@
 //
 
 #include <stdio.h>
+#include <ros/ros.h>
+
+// ROS messages
+#include <sensor_msgs/JointState.h>
+
+using namespace std;
 
 int main(int argc, char **argv){
 
-    
+    string node_name = "joint_state_dummy";
+    string topic_name = "/dvrk/PSM1/io/joint_position";
+
+    ros::init(argc, argv, node_name);
+    ros::NodeHandle nh;
+    ros::Publisher kin_data_sub = nh.advertise<sensor_msgs::JointState>(topic_name, 1);
+    ros::Rate rate(30);
+
 }
