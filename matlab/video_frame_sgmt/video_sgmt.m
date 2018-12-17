771cfcb97255eabc006e21438aa8ac20a5d03338
unknown <elaat@ELEC-PC6106.ds.leeds.ac.uk>
2018-12-17 14:36:09 +0000

    Settable image format for output frames

--- matlab/video_frame_sgmt/video_sgmt.m
+++ matlab/video_frame_sgmt/video_sgmt.m
@@ -2,12 +2,17 @@ clc
 clear all
 close all
 
-% Insert folder names of the operations to read
-op = ['cali']; % 'tons'; 'lobe'; 'cyst';
-frame_skip = 24; % number of frame to skip
-origin_folder = 'videos\compress\';
-destination_folder = 'videos\frame_0\';
+%------------------------------------------------
+% User's parameters
 
+% 'tons'; 'lobe'; 'cyst';
+op = ['cali'];                              % list of tasks
+frame_skip = 24;                            % number of frame to skip
+origin_folder = 'videos\compress\';         % origin folder of the video
+destination_folder = 'videos\frame_0\';     % destination folder for frames
+img_format = '.png';                        % image compression format
+
+%------------------------------------------------
 op = cellstr(op);
 video_path = cell(size(op,1),1);
 frames_paths = cell(size(op,1),1);
@@ -50,7 +55,7 @@ for i = 1 : size(op,1)
         % for loop over frames
         for j = 1 : frame_skip : n_frames
             frames = read(video, j);
-            title = strcat(file_folder, '\' ,'frame_', num2str(j-1),'.png');
+            title = strcat(file_folder, '\' ,'frame_', num2str(j-1), img_format);
             imwrite(frames, title);
         end
     end
