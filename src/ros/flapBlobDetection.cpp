112700bb6103af6f40a8893f349a7f4eced51442
Aleks Attanasio <aleks.attanasio@gmail.com>
2019-01-17 15:52:46 +0000

    Fixed Mat reading method for disparity map

--- src/ros/flapBlobDetection.cpp
+++ src/ros/flapBlobDetection.cpp
@@ -4,6 +4,7 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <sensor_msgs/image_encodings.h>
 #include <opencv2/features2d/features2d.hpp>
 
 
@@ -17,41 +18,16 @@ using namespace std;
 
 
 stereo_msgs::DisparityImage disp_map = stereo_msgs::DisparityImage();
-sensor_msgs::Image disp_map_img = sensor_msgs::Image();
-cv_bridge::CvImage bridge;
-
-double min_range_ = 5;
-double max_range_ = 20;
+sensor_msgs::ImageConstPtr disp_map_img;
+cv_bridge::CvImagePtr cv_ptr (new cv_bridge::CvImage);
+cv::Mat locImage;
 
 void disparityCallback(const stereo_msgs::DisparityImage msg){
+    locImage=cv_bridge::toCvCopy(msg.image)->image;
+    locImage.convertTo(locImage,CV_32FC1,1.0/255.0);
 
-    try
-    {
-        bridge = cv_bridge::toCvCopy(msg.image, "32FC1");
-
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("Failed to transform depth image.");
-        return;
-    }
-
-    // convert to something visible
-//    cv::Mat img(bridge->image.rows, bridge->image.cols, CV_8UC1);
-//    for(int i = 0; i < bridge->image.rows; i++)
-//    {
-//        float* Di = bridge->image.ptr<float>(i);
-//        char* Ii = img.ptr<char>(i);
-//        for(int j = 0; j < bridge->image.cols; j++)
-//        {
-//            Ii[j] = (char) (255*((Di[j]-min_range_)/(max_range_-min_range_)));
-//        }
-//    }
-
-    // display
-//    cv::imshow("Depth Map", img);
-//    cv::waitKey(3);
-
+    cv::imshow("Right View", locImage);
+    cv::waitKey(30);
 }
 
 int main (int argc, char** argv){
@@ -61,13 +37,11 @@ int main (int argc, char** argv){
     ros::Subscriber sub_disp_map= nh.subscribe("/stereo/disparity", 1, &disparityCallback);
     ros::Rate loop_rate(24);
 
-    cv::SimpleBlobDetector blob_detector;
-
     while(ros::ok()){
-        
-        std::vector<cv::KeyPoint> keypoints;
-//        imshow("DM", bridge->image);
+
         cout << "runnin" << endl;
+
+
         loop_rate.sleep();
         ros::spinOnce();
     }
