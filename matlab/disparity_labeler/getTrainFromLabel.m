6a4d17475414cc17cf4bfb9b604fb976b89d2b48
Aleks Attanasio <aleks.attanasio@gmail.com>
2019-02-15 10:42:19 +0000

    Retrieve training set from masks

---
+++ matlab/disparity_labeler/getTrainFromLabel.m
@@ -0,0 +1,11 @@
+function [ ] = getTrainFromLabel( masks_dir )
+%GETTRAINFROMLABEL Summary of this function goes here
+%   Detailed explanation goes here
+
+    list = dir(masks_dir);
+    
+    
+    
+
+end
+
