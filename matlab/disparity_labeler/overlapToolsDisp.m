fac9fda40f50383d858020de5b73a8a9f83aa228
Aleks Attanasio <aleks.attanasio@gmail.com>
2019-03-01 16:48:37 +0000

    Added function for to overlap tools over disparity map

---
+++ matlab/disparity_labeler/overlapToolsDisp.m
@@ -0,0 +1,18 @@
+function [ outputImg ] = overlapToolsDisp( depth_tissue, depth_tool )
+% This function takes a background tissue image and overlaps a set of tools
+% (depth_tool) over it.
+
+    for i = 1 : size(depth_tissue, 1)
+        for j = 1 : size(depth_tissue, 2)
+            
+            if (sum(depth_tool(i,j,:)) ~= 0 )
+                    
+                depth_tissue(i,j,:) = depth_tool(i,j,:); 
+                
+            end
+        end
+    end
+    
+    outputImg = depth_tissue;
+end
+
