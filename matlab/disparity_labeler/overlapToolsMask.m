f3a2e0f15c9f35f4e5488f6adb79724aa3dd4c92
Aleks Attanasio <aleks.attanasio@gmail.com>
2019-03-01 16:54:06 +0000

    Now the function handles RGB images dimensions

--- matlab/disparity_labeler/overlapToolsMask.m
+++ matlab/disparity_labeler/overlapToolsMask.m
@@ -1,16 +1,20 @@
 function [ outputImg ] = overlapToolsMask( depth_tissue, depth_tool, mask_tool )
+% This function takes a background disparity map (depth_tissue) and
+% extracts the tools out of a disparity map with tools (depth_tool) basing
+% on the tool mask (mask_tool)
 
-    for i = 1 : size(depth_tissue, 1)
-        for j = 1 : size(depth_tissue, 2)
+for i = 1 : size(depth_tissue, 1)
+    for j = 1 : size(depth_tissue, 2)
+        
+        if (mask_tool(i,j) == 255)
             
-            if (mask_tool(i,j) == 255)
-               
-                depth_tissue(i,j,:) = depth_tool(i,j,:); 
-                
-            end
+            depth_tissue(i,j,:) = depth_tool(i,j,:);
+            
         end
     end
-    
-    outputImg = depth_tissue;
+end
+
+outputImg = depth_tissue;
+
 end
 
