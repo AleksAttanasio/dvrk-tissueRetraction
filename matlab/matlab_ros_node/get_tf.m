15d89bbfc93a9fc167a1dadd850502b1e3b05db4
Aleks <aleks.attanasio@gmail.com>
2019-05-24 11:22:41 +0100

    Added script to retrieve frame transform

---
+++ matlab/matlab_ros_node/get_tf.m
@@ -0,0 +1,30 @@
+ros_master_ip='129.11.176.75';
+ros_master_port=11311;
+
+rosinit(ros_master_ip,ros_master_port);
+
+
+%%
+tf_sub = rossubscriber('/tf');
+r_tf_rcv = 0;
+l_tf_rcv = 0;
+
+while (l_tf_rcv == 0 || r_tf_rcv == 0)
+    
+    msg = receive(tf_sub,4);
+    
+    if strcmp(msg.Transforms.ChildFrameId, 'endoscope/left_camera')
+        left_tf = msg.Transforms.Transform;
+        l_tf_rcv = 1;
+    end
+
+    if strcmp(msg.Transforms.ChildFrameId, 'endoscope/right_camera')
+        right_tf = msg.Transforms.Transform;
+        r_tf_rcv = 1;
+    end
+end
+
+
+
+
+
