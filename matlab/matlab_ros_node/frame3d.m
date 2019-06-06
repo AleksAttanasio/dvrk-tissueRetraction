f1194999b7b280f3e9c53d1412051cc9004cd9b6
Aleks <aleks.attanasio@gmail.com>
2019-06-06 16:00:41 +0100

    Minor fix

--- matlab/matlab_ros_node/frame3d.m
+++ matlab/matlab_ros_node/frame3d.m
@@ -3,7 +3,6 @@ function [ ] = frame3d(origin,orientation)
 quiver3(origin(1), origin(2), origin(3), orientation(1,1), orientation(2,1) , orientation(3,1), 'color', 'g');
 hold on
 quiver3(origin(1), origin(2), origin(3), orientation(1,2), orientation(2,2) , orientation(3,2),  'color', 'r');
-hold on
 quiver3(origin(1), origin(2), origin(3), orientation(1,3), orientation(2,3) , orientation(3,3), 'color', 'b');
 
 end
