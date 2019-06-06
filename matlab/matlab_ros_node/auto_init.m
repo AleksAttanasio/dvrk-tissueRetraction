8c51aac5d27e3637c24a47631bc3b989e42e9de0
Aleks <aleks.attanasio@gmail.com>
2019-06-06 16:02:04 +0100

    First version of auto_init registration working on dvrk-lite

--- matlab/matlab_ros_node/auto_init.m
+++ matlab/matlab_ros_node/auto_init.m
@@ -1,50 +1,45 @@
 clear;
 clc;
 close all;
-addpath('../dvrk_matlab')
-
-% Define rotation of PSMs on the bases
-L_arm_rot = 0;
-R_arm_rot = 0;
+rosshutdown;
 
 % Retrieve additional information
 config_file;
 
 % rosinit(ros_master_ip,ros_master_port);
-rosinit()
+rosinit
 get_tf;
-
-T_c_o = get_camera_origin_transform(left_tf);
-
+auto_transform;
+%%
 T_c_p0 = T_c_o * T_o_pb * T_pb_p0;
-
+T_o_p0 = T_o_pb * T_pb_p0;
 %instantiate psm
 psml=psm(psmName);
 
 %set camera frame as reference frame for PSM 
-psml.set_base_frame(T_c_p0);
+psml.set_base_frame(T_o_p0);
 
 %home the PSM
-msg=strcat('Do you want to home ',psmName,'? Y/N');
-answer=input(msg,'s');
-if answer ~= 'Y'
-    exit();
-end
-psml.home();
+% msg=strcat('Do you want to home ',psmName,'? Y/N');
+% answer=input(msg,'s');
+% if answer == 'Y'
+% psml.home();    
+% end
 
 %perform test motion
-msg=strcat('Do you perform test motion on ',psmName,'? Y/N');
-answer=input(msg,'s');
-if answer ~= 'Y'
-    exit();
-end
-psml.dmove_translation([0.02,0.0,0.0]);
-wait(2)
-psml.dmove_translation([-0.02 0.02 0]);
-wait(2)
-psml.dmove_translation([0 -0.02 0.02]);
-wait(2)
-psml.dmove_translation([0 0 -0.02]);
+% msg=strcat('Do you perform test motion on ',psmName,'? Y/N');
+% answer=input(msg,'s');
+% if answer == 'Y'
+%     psml.dmove_translation([0.01,0.0,0.0]);
+% pause(2.0)
+% psml.dmove_translation([0.01 0.0 0.0]);
+% pause(2.0)
+% psml.dmove_translation([0.0 0.01 0.0]);
+% pause(2.0)
+% psml.dmove_translation([0.0 0.0 0.01]);
+% 
+% 
+% end
 
 
 
