d4d216e9d583ca354e7b7d4486470dce1ea1d8c4
Aleks <aleks.attanasio@gmail.com>
2019-09-11 16:36:38 +0100

    Updated last main used for test

--- matlab/matlab_ros_node/main_script.m
+++ matlab/matlab_ros_node/main_script.m
@@ -17,6 +17,8 @@ save last_ws.mat
 
 %%
 %instantiate psm
+load last_ws.mat
+load rot_tool.mat
 psml=psm(psmName);
 %%
 
@@ -24,7 +26,7 @@ psml=psm(psmName);
 psml.set_base_frame(T_c_p0);
 curpos=psml.get_position_current();
 tform = axang2tform([0 0 1 pi/2]);
-curpos_vertical= [[0 0 1;1 0 0;0 1 0;0 0 0], [curpos(1:3,4);1]];
+curpos_vertical= [[0 0 -1;1 0 0;0 1 0;0 0 0], [curpos(1:3,4);1]];
 
 psml.move(curpos_vertical)
 psml.close_jaw()
@@ -49,29 +51,95 @@ p=polyfit(pos(:,1),pos(:,2),1);
 %compute euclidean distance
 dist=norm(pos(:,1)-pos(:,2));
 % want to move into background tissue of 10% of distance between the points
-dist=dist*0.5;
+dist=dist*0.2;
 %compute x and y of first point
 if(pos(2,1)-pos(1,1)>0) % caso del flap a sinistra 
-        xp = pos(2,1)+dist/sqrt(1+p(1));
+        xp = pos(2,1)+dist/sqrt(2+p(1));
         yp = polyval(p,xp);
 else
-        xp = pos(2,1)-dist/sqrt(1+p(1));
+        xp = pos(2,1)-dist/sqrt(2+p(1));
         yp = polyval(p,xp);
 end
 %%
-%move to starting point but at Z closer to camera
-grasp_pos=trvec2tform([xp yp tissue_pt_msg.Point.Z-(tissue_pt_msg.Point.Z*0.5)]);
+% %move to starting point but at Z closer to camera
+grasp_pos=trvec2tform([xp yp tissue_pt_msg.Point.Z-(tissue_pt_msg.Point.Z*0.5)]);R_o_pb
 grasp_orient=rotm2tform(vertical_rotation_global);
 grasp_pose=grasp_pos*grasp_orient;
 psml.move(grasp_pose)
+
+
+pause(2)
+% graps_pos= trvec2tform([0 -0.05 0.01]);R_o_pb
+% grasp_orient=rotm2tform(rot_ver);
+% grasp_pose=grasp_pos*grasp_orient;
+% psml.move(grasp_pose)
 
 %move to same x and y but z lower
 grasp_pos=psml.get_position_current;
 grasp_pos(3,4)=(background_pt_msg.Point.Z+tissue_pt_msg.Point.Z)/2;
 psml.move(grasp_pos);
 
+pause(2)
 %move at same z to x and y of tissue point
 grasp_pos=psml.get_position_current;
 grasp_pos(1,4)=tissue_pt_msg.Point.X;
 grasp_pos(2,4)=tissue_pt_msg.Point.Y;
 psml.move(grasp_pos);
+
+
+%% if needed move more
+
+% tool_pos = psml.get_position_current
+% pos_t=[tool_pos(1,4),tool_pos(2,4);grasp_pt_msg.Point.X, grasp_pt_msg.Point.Y];
+
+pos_t=[tissue_pt_msg.Point.X,tissue_pt_msg.Point.Y;grasp_pt_msg.Point.X, grasp_pt_msg.Point.Y];
+
+p_t=polyfit(pos_t(:,1),pos_t(:,2),1);
+%compute euclidean distance
+dist_t=norm(pos_t(:,1)-pos_t(:,2));
+
+fact = 7;
+
+% %compute x and y of first point
+if(pos(2,1)-pos(1,1)>0) % caso del flap a sinistra 
+        xp_t = pos(2,1)-fact*dist_t/sqrt(1+p_t(1));
+        yp_t = polyval(p,xp);
+else
+        xp_t = pos(2,1)+fact*dist_t/sqrt(1+p_t(1));
+        yp_t = polyval(p,xp);
+end
+end_pos=trvec2tform([xp_t yp_t tissue_pt_msg.Point.Z]);R_o_pb
+grasp_pos(1,4) = end_pos(1,4);
+grasp_pos(2,4) = end_pos(2,4);
+% grasp_pos(3,4) = end_pos(3,4);
+psml.move(grasp_pos)
+%% VERTICAL RETRACtION
+% tool_pos = psml.get_position_current
+% pos_t=[tool_pos(1,4),tool_pos(2,4);grasp_pt_msg.Point.X, grasp_pt_msg.Point.Y];
+
+pos_t=[tissue_pt_msg.Point.X,tissue_pt_msg.Point.Y;grasp_pt_msg.Point.X, grasp_pt_msg.Point.Y];
+
+p_t=polyfit(pos_t(:,1),pos_t(:,2),1);
+%compute euclidean distance
+dist_t=norm(pos_t(:,1)-pos_t(:,2));
+
+fact = 1.5;
+
+
+if(pos(2,1)-pos(1,1)>0) % caso del flap a sinistra 
+        xp_t = pos(2,1);
+        yp_t = polyval(p,xp)+fact*dist_t/sqrt(2+p_t(1));
+else
+        xp_t = pos(2,1);
+        yp_t = polyval(p,xp)-fact*dist_t/sqrt(2+p_t(1));
+end
+
+
+end_pos=trvec2tform([xp_t yp_t tissue_pt_msg.Point.Z]);R_o_pb
+
+grasp_pos(1,4) = end_pos(1,4);
+grasp_pos(2,4) = end_pos(2,4);
+% grasp_pos(3,4) = end_pos(3,4);
+psml.move(grasp_pos)
+
+
